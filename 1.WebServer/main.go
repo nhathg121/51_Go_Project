05dad353ee948502f0aa43845388468ac709d93e
nhat nhat <nhatnhat@nhats-MacBook-Air.local>
2023-12-17 10:40:08 +0700

    #2

--- 1.WebServer/main.go
+++ 1.WebServer/main.go
@@ -6,27 +6,26 @@ import (
 	"net/http"
 )
 
-
-
-func main(){
+func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/form", formHandler)
 
-	fmt.Printf("Starting server on port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err!= nil {
+	fmt.Printf("Starting server on port 10000\n")
+	if err := http.ListenAndServe(":10000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
+
 // helloHandler is a handler function for the /hello route
-func helloHandler(w http.ResponseWriter, r *http.Request){
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 
 		http.Error(w, "404 Not found", http.StatusNotFound)
-		return 
+		return
 	}
-	if r.Method!= "GET"{
+	if r.Method != "GET" {
 		http.Error(w, "405 Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
@@ -34,14 +33,13 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 }
 
 // formHandler is a handler function for the /form route
-func formHandler(w http.ResponseWriter, r *http.Request){
-	if err := r.ParseForm(); err != nil{
-			fmt.Fprintf(w,"ParseForm() err: %v", err)
-			return
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
 	}
-	fmt.Fprintf(w,"POST  request successful! \n")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w,"Name =  %s\n", name)
-	fmt.Fprintf(w,"Address =  %s\n", address)
+	fmt.Fprintf(w, "POST  request successful! \n")
+	fmt.Fprintf(w, "Name: %s \n", r.FormValue("name"))
+	fmt.Fprintf(w, "address: %s \n", r.FormValue("address"))
+
 }
--- 2.CRUD_api/main.go
+++ 2.CRUD_api/main.go
@@ -1,31 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"math/rand"
-	"encoding/json"
+	"net/http"
 	"strconv"
-	"github.com/gorilla/mux"
 
+	"github.com/gorilla/mux"
 )
 
-
 type Movie struct {
-	ID string `json:"id,omitempty"`  
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	ID       string    `json:"id,omitempty"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
 type Director struct {
-		FirstName string `json:"firstName"`
-		LastName string `json:"lastName"`
-
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 }
 
 var movies []Movie
+
 // func get all movies
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -37,9 +36,9 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index,item := range movies {
+	for index, item := range movies {
 		if item.ID == params["id"] {
-movies = append(movies[:index], movies[index+1:]...)
+			movies = append(movies[:index], movies[index+1:]...)
 			break
 		}
 	}
@@ -50,7 +49,7 @@ movies = append(movies[:index], movies[index+1:]...)
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _,item := range movies {
+	for _, item := range movies {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			break
@@ -58,16 +57,42 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	json.NewDecoder(r.Body).Decode(&movie)
+	movies = append(movies, movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// set response header
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			json.NewEncoder(w).Encode(movie)
+			break
+		}
+	}
+}
 
 func main() {
 	r := mux.NewRouter()
 
-
-	movies = append(movies, Movie{ID: "1",Isbn: "111111",Title: "Movie 1",Director: &Director{FirstName: "John",LastName: "Doe"}})
-	movies = append(movies, Movie{ID: "2",Isbn: "222222",Title: "Movie 2",Director: &Director{FirstName: "John",LastName: "Doe"}})
-	movies = append(movies, Movie{ID: "3",Isbn: "333333",Title: "Movie 3",Director: &Director{FirstName: "John",LastName: "Doe"}})
-	movies = append(movies, Movie{ID: "4",Isbn: "444444",Title: "Movie 4",Director: &Director{FirstName: "John",LastName: "Doe"}})
-	movies = append(movies, Movie{ID: "5",Isbn: "555555",Title: "Movie 5",Director: &Director{FirstName: "John",LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "1", Isbn: "111111", Title: "Movie 1", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "222222", Title: "Movie 2", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "3", Isbn: "333333", Title: "Movie 3", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "4", Isbn: "444444", Title: "Movie 4", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "5", Isbn: "555555", Title: "Movie 5", Director: &Director{FirstName: "John", LastName: "Doe"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("Get")
@@ -75,8 +100,6 @@ func main() {
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
-
-
-	fmt.Printf("Starting server on port 1999\n")
-	log.Fatal(http.ListenAndServe(":1999", r))
+	fmt.Printf("Starting server on port 2000\n")
+	log.Fatal(http.ListenAndServe(":2000", r))
 }
